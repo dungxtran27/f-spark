5f24ff6c9efdad4fc047e36c2e56dbc2679d4825
hieunthe163894 <trunghieu2224@gmail.com>
2024-10-17 15:34:51 +0700

    Api be

--- be/routes/student.js
+++ be/routes/student.js
@@ -3,9 +3,10 @@ import { StudentController } from "../controller/index.js";
 import verifyToken from "../middleware/verifyToken.js";
 import authorization from "../middleware/authorization.js";
 const studentRouter = express.Router();
-studentRouter.get('/viewStudentByGroup',verifyToken, StudentController.getStudentsInSameGroup);
-studentRouter.get('/', verifyToken, authorization.checkGroupAccess, StudentController.findStudentByGroupId)
-studentRouter.get("/getTeacherByStudentId",verifyToken, StudentController.getTeacherByStudentId)
-studentRouter.get('/viewStudentByGroup',verifyToken, StudentController.getStudentsInSameGroup);
+studentRouter.get('/viewStudentByGroup', verifyToken, StudentController.getStudentsInSameGroup);
+studentRouter.get('/getAllStudentByGroupId', verifyToken, authorization.checkGroupAccess, StudentController.findAllStudentByGroupId)
+studentRouter.get("/getTeacherByStudentId", verifyToken, StudentController.getTeacherByStudentId)
+studentRouter.get('/viewStudentByGroup', verifyToken, StudentController.getStudentsInSameGroup);
+studentRouter.get('/getAllStudentByClassId', verifyToken, StudentController.getAllStudentByClassId);
 export default studentRouter;
 
