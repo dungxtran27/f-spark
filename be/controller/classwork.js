c0c3a3ca004cae74cf47a4127b8aa8b1498dcde4
huyngquang02-1633 <huy12042002yb@gmail.com>
2024-10-19 12:43:36 +0700

    huynq

--- be/controller/classwork.js
+++ be/controller/classwork.js
@@ -3,6 +3,7 @@ import {
   StudentRepository,
   SubmissionRepository,
 } from "../repository/index.js";
+import mongoose from "mongoose";
 const getClassWorkByStudent = async (req, res) => {
   try {
     const decodedToken = req.decodedToken;
@@ -46,6 +47,9 @@ const viewOutcomes = async (req, res) => {
       outcomeIds,
       student.group
     );
+    console.log(student.group);
+    console.log(outcomeIds);
+    
     const modifiedOutcome = outcomesList.map((oc) => {
       const submission = submissions.find(
         (s) => s.classworkId.toString() === oc._id.toString()
@@ -60,8 +64,22 @@ const viewOutcomes = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+const getOutcomesByTeacher = async (req,res) => {
+  try {
+    const classId = req.params.classId;
+    const id  = new mongoose.Types.ObjectId(classId)
+    const outcomes = await ClassworkRepository.getOutcomes(
+      id
+    )
+    return res.status(200).json({ data: outcomes });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
 export default {
   getClassWorkByStudent,
   getClassWorkByTeacher,
   viewOutcomes,
+  getOutcomesByTeacher
 };
--- be/routes/classwork.js
+++ be/routes/classwork.js
@@ -5,4 +5,5 @@ const classWorkRouter = express.Router();
 classWorkRouter.get("/getClassWorkByStudent/:type", verifyToken,ClassworkController.getClassWorkByStudent)
 classWorkRouter.get("/getClassWorkByTeacher/:classId/:type", verifyToken, ClassworkController.getClassWorkByTeacher)
 classWorkRouter.get("/viewOutcomes", verifyToken, ClassworkController.viewOutcomes)
+classWorkRouter.get("/getOutcomesByTeacher/:classId",ClassworkController.getOutcomesByTeacher) //get all outcome theo lớp màn outcome-teacher
 export default classWorkRouter;
