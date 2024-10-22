223ede2343edd66a07602ac7f9a2215b53cdaebd
hieunthe163894 <trunghieu2224@gmail.com>
2024-10-23 00:16:56 +0700

    fix

--- be/routes/tagmajor.js
+++ be/routes/tagmajor.js
@@ -1,9 +1,7 @@
 import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
 import { TagMajorController } from "../controller/index.js";
-const classRouter = express.Router();
-
-
-classRouter.get('/getAllTagMajor', TagMajorController.getAllMajor);
+const tagMajorRouter = express.Router();
+tagMajorRouter.get('/getAllTagMajor', TagMajorController.getAllMajor);
 
 export default classRouter;
