64055ea210649fbe2aa96f13218f2dabe8219e1b
Mia2114 <chuvietthang0101@gmail.com>
2024-11-01 01:19:34 +0700

    thang

--- fe/src/component/pdt/ManageAccount/MentorProfile/index.tsx
+++ fe/src/component/pdt/ManageAccount/MentorProfile/index.tsx
@@ -37,66 +37,73 @@ const MentorProfileWrapper = () => {
     ];
 
     return (
-        <div className="p-4">
-            <Divider orientation="left">Mentor information</Divider>
-            <div className="flex items-start space-x-4 mb-4">
-                <img
-                    src="https://via.placeholder.com/150"
-                    alt="Mentor Avatar"
-                    className="rounded-full w-32 h-32"
-                />
-                <div>
-                    <Title level={2}>Nguyễn Văn ssss</Title>
-                    <Text type="secondary" className="block mb-2">
-                        Mentor chuyên ngành Phát triển phần mềm với hơn 10 năm kinh nghiệm, hướng dẫn và đào tạo hàng trăm lập trình viên thành công.
-                    </Text>
-                    <Space className="mb-2">
-                        <Text>Email: nguyen.van.a@example.com</Text>
-                        <Text>SĐT: 0123 456 789</Text>
-                        <Text>
-                            LinkedIn:{" "}
-                            <a href="" target="_blank" rel="noopener noreferrer">
-                                Nguyễn Văn BS
-                            </a>
+        <div className="p-0.5 bg-white rounded-lg shadow-md">
+            <div className=" p-4 ">
+                <Divider orientation="left">Mentor information</Divider>
+                <div className="flex items-start space-x-4 mb-4">
+                    <img
+                        src="https://via.placeholder.com/150"
+                        alt="Mentor Avatar"
+                        className="rounded-full w-32 h-32"
+                    />
+                    <div>
+                        <Title level={2}>Nguyễn Văn ssss</Title>
+                        <Text type="secondary" className="block mb-2">
+                            Mentor chuyên ngành Phát triển phần mềm với hơn 10 năm kinh nghiệm, hướng dẫn và đào tạo hàng trăm lập trình viên thành công.
                         </Text>
-                    </Space>
-                    <Space className="flex mt-2">
-                        Specific:
-                        <Tag color="blue">SE</Tag>
-                        <Tag color="green">KS</Tag>
-                        <Tag color="orange">FS</Tag>
-                    </Space>
+                        <Space className="mb-2">
+                            <Text>Email: nguyen.van.a@example.com</Text>
+                            <Text>SĐT: 0123 456 789</Text>
+                            <Text>
+                                LinkedIn:{" "}
+                                <a href="" target="_blank" rel="noopener noreferrer">
+                                    Nguyễn Văn BS
+                                </a>
+                            </Text>
+                        </Space>
+                        <Space className="flex mt-2">
+                            Specific:
+                            <Tag color="blue">SE</Tag>
+                            <Tag color="green">KS</Tag>
+                            <Tag color="orange">FS</Tag>
+                        </Space>
+                    </div>
                 </div>
-            </div>
 
-            <Divider orientation="left">Support Group - {groups.length} groups</Divider>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {groups.map((group, index) => (
-                    <Card key={index} className="rounded-md overflow-hidden shadow-md cursor-pointer mb-4" hoverable bodyStyle={{ padding: 0 }}>
-                        <div className="bg-red-500 p-4 text-white">
-                            <div className="flex items-center">
-                                <GroupOutlined className="text-white mr-2" />
-                                <div className="text-lg font-semibold">{group.groupName}</div>
-                            </div>
-                        </div>
-                        <div className="bg-white p-4">
-                            <div className="flex flex-col">
-                                <div className="flex items-center mb-2">
-                                    <ProjectOutlined className="text-red-500 mr-2" />
-                                    <span className="text-sm font-medium">{group.projectTitle}</span>
-                                </div>
-                                <div className="flex items-center mb-2">
-                                    <HomeOutlined className="text-red-500 mr-2" />
-                                    <span className="text-sm">{group.className}</span>
-                                </div>
+                <Divider orientation="left">Support Group - {groups.length} groups</Divider>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {groups.map((group, index) => (
+                        <Card 
+                            key={index} 
+                            className="rounded-md overflow-hidden shadow-md cursor-pointer mb-4" 
+                            hoverable 
+                            bodyStyle={{ padding: 0 }}
+                        >
+                            <div className="bg-red-500 p-4 text-white">
                                 <div className="flex items-center">
-                                    <UsergroupAddOutlined className="text-red-500 mr-2" />
-                                    <span className="text-sm font-medium">{group.members.length} Members</span>
+                                    <GroupOutlined className="text-white mr-2" />
+                                    <div className="text-lg font-semibold">{group.groupName}</div>
                                 </div>
                             </div>
-                        </div>
-                    </Card>
-                ))}
+                            <div className="bg-white p-4">
+                                <div className="flex flex-col">
+                                    <div className="flex items-center mb-2">
+                                        <ProjectOutlined className="text-red-500 mr-2" />
+                                        <span className="text-sm ">{group.projectTitle}</span>
+                                    </div>
+                                    <div className="flex items-center mb-2">
+                                        <HomeOutlined className="text-red-500 mr-2" />
+                                        <span className="text-sm">{group.className}</span>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <UsergroupAddOutlined className="text-red-500 mr-2" />
+                                        <span className="text-sm ">{group.members.length} Members</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    ))}
+                </div>
             </div>
         </div>
     );
