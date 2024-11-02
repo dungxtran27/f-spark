3e03a1762cf8fe255ec9e4fb8fd7abf0fb60ad5d
dungxtran <dungxtran27@gmail.com>
2024-11-02 17:34:27 +0700

    khung

---
+++ fe/src/component/pdt/ManageRequest/index.tsx
@@ -0,0 +1,4 @@
+const RequestWrapper = () => {
+  return <></>;
+};
+export default RequestWrapper;
---
+++ fe/src/pages/PDT/manageRequest/index.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from "../../../layout/DefaultLayout";
+import RequestWrapper from "../../../component/pdt/ManageRequest";
+const ManageClass = () => {
+  return (
+    <DefaultLayout>
+      <RequestWrapper />
+    </DefaultLayout>
+  );
+};
+export default ManageClass;
