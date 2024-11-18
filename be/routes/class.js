e0c1472bf43b070e42aa8e6af3a02511064fd3a2
Mia2114 <chuvietthang0101@gmail.com>
2024-11-19 01:39:16 +0700

    "Remove package-lock.json file"

--- be/routes/class.js
+++ be/routes/class.js
@@ -2,9 +2,8 @@ import express from "express";
 import { ClassController } from "../controller/index.js";
 import verifyToken from "../middleware/verifyToken.js";
 const classRouter = express.Router();
-classRouter.get("/getTeacherClasses", verifyToken,ClassController.getClassesOfTeacher)
+classRouter.get("/getTeacherClasses", verifyToken, ClassController.getClassesOfTeacher)
 classRouter.put('/pinClasswork', verifyToken, ClassController.pinClasswork);
 classRouter.get("/classes", ClassController.getAllClasses);
-
 classRouter.post('/getAllClass', ClassController.getAllClass);
 export default classRouter;
