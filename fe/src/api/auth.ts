252cdaea3a064f267cc1c8142e8fcdb6648f1e62
Mia2114 <chuvietthang0101@gmail.com>
2024-12-12 21:18:55 +0700

    gg login

--- fe/src/api/auth.ts
+++ fe/src/api/auth.ts
@@ -15,8 +15,8 @@ export const authApi = {
   logOut: async () => {
     return await axios.get("api/auth/logOut");
   },
-  googleLogin: async (token: string) => {
-    return await axios.post("api/auth/googleLogin", { token });
+  googleLogin: async (token: string, role: string) => {
+    return await axios.post("api/auth/googleLogin", { token, role });
   },
   getActiveTerm: async () => {
     return await axios.post("api/term/active");
