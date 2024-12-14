39e5333209c05d2b50d4056937a323c69683748e
hieunthe163894 <trunghieu2224@gmail.com>
2024-12-14 21:17:36 +0700

    fix

--- fe/src/utils/const.tsx
+++ fe/src/utils/const.tsx
@@ -205,11 +205,6 @@ export const OVERVIEW_TABS = [
   { key: "overview", label: "Overview", children: <Overview /> },
   { key: "outcome", label: "Outcome", children: <Outcome /> },
 ];
-export const MANAGEACCOUNT_TABS = [
-  { key: "student", label: "Student", children: <AccountManagement /> },
-  { key: "teacher", label: "Teacher", children: <Teacher /> },
-  { key: "mentor", label: "Mentor", children: <Mentor /> },
-];
 export const TASK_TYPE = {
   CLASS_WORK: "Class work",
   GROUP_WORK: "Group task",
